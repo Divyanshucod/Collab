95a9c854feec0ac03c2fc8d3c082f04800f5311c
Divyanshucod <prajapatidivyanshu90@gmail.com>
2025-01-26 12:42:38 +0530

    setting up the editor

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Editor } from './editor';
 interface DocumentIdPageProps {
     params : Promise<{documentId : string}>
 }
 export default async function page({params}:DocumentIdPageProps) {
     const {documentId} = await params;
   return (
-    <div>page: {documentId}</div>
+    <div className='min-h-screen bg-[#FAFBFD]'>
+    <Editor/>
+    </div>
   )
 }
