f4c8f00000f7c75f8770b42b94e7fb6e04481cde
Divyanshucod <prajapatidivyanshu90@gmail.com>
2025-01-27 19:19:16 +0530

    editor setup is done adding toolbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Editor } from './editor';
+import { Toolbar } from './toolbar';
 interface DocumentIdPageProps {
     params : Promise<{documentId : string}>
 }
@@ -7,6 +8,7 @@ export default async function page({params}:DocumentIdPageProps) {
     const {documentId} = await params;
   return (
     <div className='min-h-screen bg-[#FAFBFD]'>
+      <Toolbar/>
     <Editor/>
     </div>
   )
